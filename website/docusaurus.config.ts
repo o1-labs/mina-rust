9f0c6b84cce2119c42def1aeeba7339931f8aadd
Danny Willems <be.danny.willems@gmail.com>
2025-07-30 10:12:53 +0200

    Docs; replace social card

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -67,8 +67,7 @@ const config: Config = {
 
 
   themeConfig: {
-    // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/openmina-social-card.svg',
 
     // SEO improvements and metadata
     metadata: [
