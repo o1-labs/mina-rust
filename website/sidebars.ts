f858bc4cfaaaedf04a5a908efc0194f3950d77f8
Danny Willems <be.danny.willems@gmail.com>
2025-08-13 19:19:18 +0200

    Website: move p2p related doc to their own section

--- website/sidebars.ts
+++ website/sidebars.ts
@@ -73,7 +73,15 @@ const sidebars: SidebarsConfig = {
       items: [
         'developers/why-openmina',
         'developers/architecture',
+      ],
+    },
+    {
+      type: 'category',
+      label: 'P2P Networking',
+      items: [
+        'developers/p2p-networking',
         'developers/webrtc',
+        'developers/libp2p',
       ],
     },
   ],
