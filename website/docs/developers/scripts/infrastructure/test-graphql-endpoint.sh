abb72ce8569e40305a2f194db8850491e7aeea3b
Danny Willems <be.danny.willems@gmail.com>
2025-09-19 11:17:08 +0200

    Website/infrastructure: escape double quotes and remove new lines

--- website/docs/developers/scripts/infrastructure/test-graphql-endpoint.sh
+++ website/docs/developers/scripts/infrastructure/test-graphql-endpoint.sh
@@ -12,7 +12,9 @@ fi
 GRAPHQL_ENDPOINT="$1"
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-QUERY=$(cat "$SCRIPT_DIR/queries/schema-introspection.graphql")
+# Read the query and create JSON payload using jq for proper escaping
+QUERY=$(< "$SCRIPT_DIR/queries/schema-introspection.graphql")
+JSON_PAYLOAD=$(echo "{}" | jq --arg query "$QUERY" '.query = $query')
 curl -s -X POST "$GRAPHQL_ENDPOINT" \
   -H "Content-Type: application/json" \
-  -d "{\"query\": \"$QUERY\"}"
+  -d "$JSON_PAYLOAD"
