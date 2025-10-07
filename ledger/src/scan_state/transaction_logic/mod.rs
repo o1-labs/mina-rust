3e8b2b300e7e3a7dd97fa51f334be38131f9ccfc
Danny Willems <be.danny.willems@gmail.com>
2025-10-07 18:24:09 +0200

    Ledger/transaction_logic: extract zkapp_statement module to separate file
    Extract the zkapp_statement module from transaction_logic/mod.rs into its
    own file. This module contains the TransactionCommitment and ZkappStatement
    types used for zkApp transaction commitments.
    
    Changes:
    - Extract zkapp_statement module to transaction_logic/zkapp_statement.rs
    - Use explicit imports instead of 'use super::*'
    - Import zkapp_command module as 'self' to access AccountUpdateRef trait
    - Update mod.rs to reference the new module file

--- ledger/src/scan_state/transaction_logic/mod.rs
+++ ledger/src/scan_state/transaction_logic/mod.rs
@@ -6,14 +6,14 @@ use std::{
 use ark_ff::Zero;
 use itertools::{FoldWhile, Itertools};
 use mina_core::constants::ConstraintConstants;
-use mina_hasher::{Fp, Hashable, ROInput};
+use mina_hasher::Fp;
 use mina_macros::SerdeYojsonEnum;
 use mina_p2p_messages::{
     bigint::InvalidBigInt,
     binprot,
     v2::{MinaBaseUserCommandStableV2, MinaTransactionTransactionStableV2},
 };
-use mina_signer::{CompressedPubKey, NetworkId};
+use mina_signer::CompressedPubKey;
 use poseidon::hash::{
     hash_with_kimchi,
     params::{CODA_RECEIPT_UC, MINA_ZKAPP_MEMO},
@@ -608,103 +608,7 @@ impl Memo {
 pub mod signed_command;
 
 pub mod zkapp_command;
-pub mod zkapp_statement {
-    use poseidon::hash::params::MINA_ACCOUNT_UPDATE_CONS;
-
-    use super::{
-        zkapp_command::{CallForest, Tree},
-        *,
-    };
-
-    #[derive(Copy, Clone, Debug, derive_more::Deref, derive_more::From)]
-    pub struct TransactionCommitment(pub Fp);
-
-    impl TransactionCommitment {
-        /// <https://github.com/MinaProtocol/mina/blob/3753a8593cc1577bcf4da16620daf9946d88e8e5/src/lib/mina_base/zkapp_command.ml#L1365>
-        pub fn create(account_updates_hash: Fp) -> Self {
-            Self(account_updates_hash)
-        }
-
-        /// <https://github.com/MinaProtocol/mina/blob/3753a8593cc1577bcf4da16620daf9946d88e8e5/src/lib/mina_base/zkapp_command.ml#L1368>
-        pub fn create_complete(&self, memo_hash: Fp, fee_payer_hash: Fp) -> Self {
-            Self(hash_with_kimchi(
-                &MINA_ACCOUNT_UPDATE_CONS,
-                &[memo_hash, fee_payer_hash, self.0],
-            ))
-        }
-
-        pub fn empty() -> Self {
-            Self(Fp::zero())
-        }
-    }
-
-    impl Hashable for TransactionCommitment {
-        type D = NetworkId;
-
-        fn to_roinput(&self) -> ROInput {
-            let mut roi = ROInput::new();
-            roi = roi.append_field(self.0);
-            roi
-        }
-
-        fn domain_string(network_id: NetworkId) -> Option<String> {
-            match network_id {
-                NetworkId::MAINNET => mina_core::network::mainnet::SIGNATURE_PREFIX,
-                NetworkId::TESTNET => mina_core::network::devnet::SIGNATURE_PREFIX,
-            }
-            .to_string()
-            .into()
-        }
-    }
-
-    #[derive(Clone, Debug)]
-    pub struct ZkappStatement {
-        pub account_update: TransactionCommitment,
-        pub calls: TransactionCommitment,
-    }
-
-    impl ZkappStatement {
-        pub fn to_field_elements(&self) -> Vec<Fp> {
-            let Self {
-                account_update,
-                calls,
-            } = self;
-
-            vec![**account_update, **calls]
-        }
-
-        pub fn of_tree<AccUpdate: Clone + zkapp_command::AccountUpdateRef>(
-            tree: &Tree<AccUpdate>,
-        ) -> Self {
-            let Tree {
-                account_update: _,
-                account_update_digest,
-                calls,
-            } = tree;
-
-            Self {
-                account_update: TransactionCommitment(account_update_digest.get().unwrap()),
-                calls: TransactionCommitment(calls.hash()),
-            }
-        }
-
-        pub fn zkapp_statements_of_forest_prime<Data: Clone>(
-            forest: CallForest<(AccountUpdate, Data)>,
-        ) -> CallForest<(AccountUpdate, (Data, Self))> {
-            forest.map_with_trees_to(|(account_update, data), tree| {
-                (account_update.clone(), (data.clone(), Self::of_tree(tree)))
-            })
-        }
-
-        fn zkapp_statements_of_forest(
-            forest: CallForest<AccountUpdate>,
-        ) -> CallForest<(AccountUpdate, Self)> {
-            forest.map_with_trees_to(|account_update, tree| {
-                (account_update.clone(), Self::of_tree(tree))
-            })
-        }
-    }
-}
+pub mod zkapp_statement;
 
 pub mod verifiable {
     use std::ops::Neg;
