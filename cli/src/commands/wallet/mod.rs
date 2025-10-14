1ce9d6a1ac2e3c33bbcfc483ed3f6c3e95bb4681
Danny Willems <be.danny.willems@gmail.com>
2025-10-14 19:54:27 +0200

    Wallet: add subcommand generate

--- cli/src/commands/wallet/mod.rs
+++ cli/src/commands/wallet/mod.rs
@@ -1,4 +1,5 @@
 pub mod address;
+pub mod generate;
 pub mod send;
 
 #[derive(Debug, clap::Args)]
@@ -9,17 +10,20 @@ pub struct Wallet {
 
 #[derive(Debug, clap::Subcommand)]
 pub enum WalletCommand {
-    /// Send a payment transaction
-    Send(send::Send),
     /// Get the address from an encrypted key file
     Address(address::Address),
+    /// Generate a new encrypted key pair
+    Generate(generate::Generate),
+    /// Send a payment transaction
+    Send(send::Send),
 }
 
 impl Wallet {
     pub fn run(self) -> anyhow::Result<()> {
         match self.command {
-            WalletCommand::Send(cmd) => cmd.run(),
             WalletCommand::Address(cmd) => cmd.run(),
+            WalletCommand::Generate(cmd) => cmd.run(),
+            WalletCommand::Send(cmd) => cmd.run(),
         }
     }
 }
